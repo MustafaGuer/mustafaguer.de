cd9ff3ab771f3adff842db189940620833c83268
Mustafa Guer <mustafaguer@outlook.com>
2021-09-21 03:01:48 +0200

    fixed projects

--- src/app/projects-javascript/projects-javascript.component.ts
+++ src/app/projects-javascript/projects-javascript.component.ts
@@ -14,24 +14,24 @@ export class ProjectsJavascriptComponent implements OnInit {
       'description': 'Javascript based game',
       'link': 'https://www.mustafaguer.de/jsprojects/Sharkie/index.html'
     },
-    {
-      'id': 'burritocompany',
-      'name': 'Burrito Company',
-      'description': 'Javascript based Object-Oriented Restaurant Website',
-      'link': 'https://www.mustafaguer.de/jsprojects/burrito-company/index.html'
-    },
+    // {
+    //   'id': 'burritocompany',
+    //   'name': 'Burrito Company',
+    //   'description': 'Javascript based Object-Oriented Restaurant Website',
+    //   'link': 'https://www.mustafaguer.de/jsprojects/burrito-company/index.html'
+    // },
     {
       'id': 'quizapp',
       'name': 'QuizApp',
       'description': 'Javascript based Quiz Game',
       'link': 'https://www.mustafaguer.de/jsprojects/Quizapp%20Dark/index.html'
     },
-    {
-      'id': 'postserver',
-      'name': 'Post API',
-      'description': 'Javascript based Web Component for searching addresses',
-      'link': 'https://www.mustafaguer.de/jsprojects/postserver-webkcomponent/index.html'
-    }
+    // {
+    //   'id': 'postserver',
+    //   'name': 'Post API',
+    //   'description': 'Javascript based Web Component for searching addresses',
+    //   'link': 'https://www.mustafaguer.de/jsprojects/postserver-webkcomponent/index.html'
+    // }
   ];
 
 
